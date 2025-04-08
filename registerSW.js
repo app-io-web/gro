c688011234494d658da58e1aaa6f583cd3e1b57b
app-io-web <app-io-web@users.noreply.github.com>
2025-04-08 19:56:52 +0000

    deploy: 879aa8a9da0a234b31354fd6d50482b0140d24cf

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gro/sw.js', { scope: '/gro/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
